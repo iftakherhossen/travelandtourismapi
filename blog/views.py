610ac0b6c88388b1cb30c6021f955839b936019a
Iftakher Hossen <iftakher.one@gmail.com>
2024-12-29 05:40:54 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,11 @@ from .serializers import BlogSerializer
 # Create your views here.
 class BlogViewset(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
+    serializer_class = BlogSerializer 
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        slug = self.request.query_params.get('slug', None)
+
+        if slug:
+            queryset = queryset.filter(slug=slug)
